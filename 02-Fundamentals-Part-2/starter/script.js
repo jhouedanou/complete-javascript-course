8ab40a813abeafa8653f2ea113f2b34787912097
Jean Luc Houedanou <jeanluc@houedanou.com>
2024-09-03 14:41:45 +0000

    d

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,27 @@
-'use strict';
-let hasDriversLicense = false;
-const passTest = true;
-if (passTest) hasDriverLicense = true
-if(hasDriversLicense) console.log("I can drive :D");
+function logger(){
+    console.log('my name is');
+}
+logger();
+
+//calling//running
+  function fruitProcessor(apples, oranges){
+      const juice = "Juice with "+apples+"appples and "+oranges;
+      return juice;
+  }
+  //explain 
+  const appleJuice = fruitProcessor(5,0);
+  console.log(appleJuice);
+  //declaration and expression
+//declaration
+  function calculdelAge1(anneeDeLaNaissance){
+    //get current year with date object
+    return (new Date().getFullYear()) - anneeDeLaNaissance;
+  }
+  const age1 = calculdelAge1(1996);
+  //expression, which produces a value 
+const calculdelAge2 =  function(anneedenaissance){
+    return (new Date().getFullYear()) - anneedenaissance;
+  }
+  const age2 = calculdelAge2(1984);
+  console.log(age1,age2);
+
